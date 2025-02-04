e1407cfbea06b670b053bd4dfaa19394fb0ca7b6
YellowVEVO <ahmadm708@gmail.com>
2025-02-03 17:33:35 -0700

    fixes for server

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use Render-assigned port
 
 app.use(bodyParser.json());
 
+// Default route to check if the server is running
+app.get('/', (req, res) => {
+  res.send("Echo Server is running!");
+});
+
+// Echo route
 app.post('/echo', (req, res) => {
   const receivedText = req.body.text;
-  res.send(receivedText);
+  res.json({ message: receivedText });
 });
 
 app.listen(port, () => {
