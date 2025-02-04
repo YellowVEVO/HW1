fb98e593d7d74a0ee7584e5ecf7dae3473c53466
YellowVEVO <ahmadm708@gmail.com>
2025-02-03 18:22:00 -0700

    wrong version

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use Render-assigned port
 
 app.use(bodyParser.json());
 
+// Default route to check if the server is running
+app.get('/', (req, res) => {
+  res.send("Echo Server is running!");
+});
+
+// Echo route
 app.post('/echo', (req, res) => {
   const receivedText = req.body.text;
-  res.send(receivedText);
+  res.json({ message: receivedText });
 });
 
 app.listen(port, () => {
-  console.log(`Echo server listening at http://localhost:${port}`);
+  console.log(Echo server listening at http://localhost:${port});
 });
