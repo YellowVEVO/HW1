8850ff38b8b8880d032de73a69104fd9efdcf66e
YellowVEVO <ahmadm708@gmail.com>
2025-02-03 18:12:17 -0700

    going back again

--- server.js
+++ server.js
@@ -2,21 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send("Echo Server is running!");
-});
-
-app.post('/', (req, res) => {
+app.post('/echo', (req, res) => {
   const receivedText = req.body.text;
-  res.json({ message: receivedText });
+  res.send(receivedText);
 });
 
 app.listen(port, () => {
   console.log(`Echo server listening at http://localhost:${port}`);
 });
-
-module.exports = app;
